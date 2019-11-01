a96a25947d40bcb74c61fa6c195d047a46711e00
lyndsiWilliams <kcatgirl@gmail.com>
2019-11-01 09:01:45 -0500

    Got everything working, just need to figure out what the error is at line 103 in index.js

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,11 @@
            https://api.github.com/users/<your name>
 */
 
-axios.get("https://api.github.com/users/lyndsiWilliams");
+// axios.get("https://api.github.com/users/lyndsiWilliams")
+//     .then((response) => {
+//         console.log(response);
+//         createComponent(response);
+//     })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -15,6 +19,10 @@ axios.get("https://api.github.com/users/lyndsiWilliams");
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
+axios.get(`https://api.github.com/users/lyndsiWilliams`)
+    .then((response) => {
+        createComponent(response);
+    })
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -26,7 +34,14 @@ axios.get("https://api.github.com/users/lyndsiWilliams");
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['ArianaShackelford', 'tygedavis', 'SeeStephSay', 'MatthewCebenka'];
+
+followersArray.forEach(follower => {
+    axios.get(`https://api.github.com/users/${follower}`)
+        .then((response) => {
+            createComponent(response);
+        })
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -51,8 +66,10 @@ const followersArray = [];
 
 
 // Grab parent element
+const cards = document.querySelector('.cards');
 
-// Loop through data
+// // Loop through data
+cards.appendChild(createComponent(cards.gitLink));
 
 // Component function
 
@@ -72,14 +89,36 @@ function createComponent(gitLink) {
 
 
     // Appending children to their parents
+    card.append(img, cardInfo);
+    cardInfo.append(H3name, username, location, profile, followers, following, bio);
+
 
     // Class names
+    card.classList.add('card');
+    H3name.classList.add('name');
+    username.classList.add('username');
+
 
     // Content
+    img.src = gitLink.data.avatar_url;
+    H3name.textContent = gitLink.data.name;
+    username.textContent = gitLink.data.login;
+    location.textContent = "Location: " + gitLink.data.location;
+    followers.textContent = "Followers: " + gitLink.data.followers
+    following.textContent = "Following: " + gitLink.data.following
+    bio.textContent = "Bio: " + gitLink.data.bio
+    profile.textContent = `Profile: `
+    gitLink.href = gitLink.data.html_url;
+    gitLink.textContent = gitLink.data.html_url;
+
+
+    console.log(gitLink)
+    document.querySelector('.cards').appendChild(card);
 
     // Events
 
-    return card;
+
+    // return card;
 }
 
 
